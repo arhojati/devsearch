7ee1bd1202324720f6339400a6a0e5dd7afa01e8
Alireza <arhodjjati@gmail.com>
2023-11-24 17:37:18 +0100

    Section 3 - 10

--- projects/views.py
+++ projects/views.py
@@ -1,32 +1,12 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .models import Project
 
 def projects(request):
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
-    project_obj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            project_obj = i
+    project_obj = Project.objects.get(id=pk)
     context = {"project": project_obj}
     return render(request, 'projects/single-project.html', context)
