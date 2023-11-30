378f2ce5157d58914e4ce423dad2a5e18c6c0fc6
Alireza <arhodjjati@gmail.com>
2023-11-30 17:55:15 +0100

    Section 7 - 20

--- users/views.py
+++ users/views.py
@@ -31,7 +31,7 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.error(request, "User logged out")
+    messages.info(request, "User logged out")
     return redirect("login")
 
 
