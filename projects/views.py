f0b937e929e0055c51752e77126f1e09aec47bbd
Alireza <arhodjjati@gmail.com>
2023-12-01 21:10:01 +0100

    Section 8 - 23

--- projects/views.py
+++ projects/views.py
@@ -15,20 +15,22 @@ def project(request, pk):
 
 @login_required(login_url='login')
 def create_project(request):
+    profile = request.user.profile
     form = ProjectForm
-    
     if request.method == 'POST':
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
             return redirect('projects')
-
     context = {'form': form}
     return render(request, 'projects/project_form.html', context)
 
 @login_required(login_url='login')
 def update_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     form = ProjectForm(instance=project)
     
     if request.method == 'POST':
@@ -42,7 +44,8 @@ def update_project(request, pk):
 
 @login_required(login_url='login')
 def delete_project(request, pk):
-    project = Project.objects.get(id=pk)
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
     context = {'object': project}
     if request.method == 'POST':
         project.delete()
