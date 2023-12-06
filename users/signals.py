1182d2181e0d28995ff12d84fef1aa0dbefb7f11
Alireza <arhodjjati@gmail.com>
2023-12-06 14:37:22 +0100

    Section 11 - 30 and 31

--- users/signals.py
+++ users/signals.py
@@ -1,5 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Profile
 # from django.dispatch import receiver
 
@@ -14,7 +16,15 @@ def create_profile(sender, instance, created, **kwargs):
             email=user.email,
             name=user.first_name
         )
-        
+        subject = 'Welcome to DevSearch'
+        message = 'we are glad you are here'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
 post_save.connect(create_profile, sender=User)
 
 def update_user(sender, instance, created, **kwarg):
