d8ebb5bbafc55c4ba39680e50d07f156c2f9fccf
Alireza <arhodjjati@gmail.com>
2023-12-07 09:38:38 +0100

    Section 12 - 37

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from projects.models import Project
+from projects.models import Project, Review
 
 
 @api_view(["GET"])
@@ -29,4 +29,21 @@ def get_projects(request):
 def get_project(request, pk):
     projects = Project.objects.get(id=pk)
     serializer = ProjectSerializer(projects, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def project_vote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+    review.value = data['value']
+    review.save()
+    project.get_vote_count
+    serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
